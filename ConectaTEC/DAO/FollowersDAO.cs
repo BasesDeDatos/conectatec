01457a6eedd4ada2cc7cbaa309af60f44b7e7baf
Jeffrey Alvarado <jefalva10296@gmail.com>
2016-04-26 23:48:27 -0600

    Initial commit

---
+++ ConectaTEC/DAO/FollowersDAO.cs
@@ -0,0 +1,35 @@
+﻿using ConectaTEC.Models;
+using Cassandra;
+using Cassandra.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConectaTEC.DAO
+{
+    public interface IFollowersDAO
+    {
+        Task<IEnumerable<FollowersModel>> getFollowers();
+    }
+
+    public class FollowersDAO : IFollowersDAO
+    {
+        protected readonly ISession session;
+        protected readonly IMapper mapper;
+
+        public FollowersDAO()
+        {
+            ICassandraDAO cassandraDAO = new CassandraDAO();
+            session = cassandraDAO.GetSession();
+            mapper = new Mapper(session);
+        }
+
+        public async Task<IEnumerable<FollowersModel>> getFollowers()
+        {
+            return await mapper.FetchAsync<FollowersModel>();
+        }
+    }
+}
---
+++ ConectaTEC/DAO/TweetsDAO.cs
@@ -0,0 +1,35 @@
+﻿using ConectaTEC.Models;
+using Cassandra;
+using Cassandra.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConectaTEC.DAO
+{
+    public interface ITweetsDAO
+    {
+        Task<IEnumerable<TweetsModel>> getTweets();
+    }
+
+    public class TweetsDAO : ITweetsDAO
+    {
+        protected readonly ISession session;
+        protected readonly IMapper mapper;
+
+        public TweetsDAO()
+        {
+            ICassandraDAO cassandraDAO = new CassandraDAO();
+            session = cassandraDAO.GetSession();
+            mapper = new Mapper(session);
+        }
+
+        public async Task<IEnumerable<TweetsModel>> getTweets()
+        {
+            return await mapper.FetchAsync<TweetsModel>();
+        }
+    }
+}
