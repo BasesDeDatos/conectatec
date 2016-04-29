a057a8883fd97b4dc4c4113dd9bb34f1b1c85efe
Jeffrey Alvarado <jefalva10296@gmail.com>
2016-04-28 21:34:36 -0600

    Initial commit

--- ConectaTEC/Controllers/HomeController.cs
+++ ConectaTEC/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace ConectaTEC.Controllers
             return View();
         }
 
+        public ActionResult Perfil()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
