d38c336fbb87c129a447ceaeda3b4a7c3cb432e0
Jeffrey Alvarado <jefalva10296@gmail.com>
2016-05-05 13:33:10 -0600

    Registro provisional

--- ConectaTEC/Controllers/AccountController.cs
+++ ConectaTEC/Controllers/AccountController.cs
@@ -188,29 +188,12 @@ namespace ConectaTEC.Controllers
             if (ModelState.IsValid)
             {
                 //var user = new UsersModel { username = model.Email, password = model.Password };
-                try {
-                    usersDao.insertUser(model.Email, model.Password);
-                }
-
-                catch (Exception ex)
+                try
                 {
+                    session.Execute("insert into users (username, name, description, email, password) values ( '" + model.Email + "', '" + model.Email + "', 'Hola, estoy en ConectaTEC', '" + model.Email + "', '" + model.Password + "' )");
                 }
-                
-                /*
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
-                */
+                catch { }
+                return RedirectToAction("Login");
             }
 
             // If we got this far, something failed, redisplay form
